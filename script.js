09a8fe8d472f8a9692084e43423abefc95fdd15e
quevon <quevonmontojo@gmail.com>
2022-02-18 15:30:17 +0800

    refactoring footer

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Software Engineer", "Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
