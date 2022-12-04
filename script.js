0fdadfad526f0398cb4a047acc523a0213354f4d
quevon <quevonmontojo@gmail.com>
2022-12-04 09:07:02 +0800

    update typing

--- script.js
+++ script.js
@@ -39,14 +39,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: [" Front End Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: [" Front End Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
